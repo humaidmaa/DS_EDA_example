78c0083d6bd31c8a11b60d583819d08e28829d3f
Mansour Alhumaid <humaidmaa@gmail.com>
2020-11-12 13:58:36 +0300

    organizing Repo

--- chickwts.R
+++ chickwts.R
@@ -1,3 +1,4 @@
+#loading library
 library(tidyverse)
 #read.csv(chickwts)
 #chickwts <- read_csv ("chickwts.csv")
@@ -5,17 +6,38 @@ glimpse(chickwts)
 summary(chickwts)
 plot(chickwts)
 
-chickwts %>%
+chickwts %>% 
   group_by(feed) %>%
-  summarise(n = n(), Avg=mean(weight), SD = sd(weight))
+  summarise(n = n(),
+            average = mean(weight),
+            SD = sd(weight))
 
 
+#box plot of feed vs weight
+ggplot(chickwts, aes( x = feed, y =  weight )) + 
+  geom_boxplot()
 
+#box plot of feed vs weight
 ggplot(chickwts,aes(x=feed, y=weight)) + 
   geom_jitter() + 
   stat_summary(fun.data = mean_sdl,
                fun.args = list(mult=1),
                color="red")
+
+
+# Inferential Statistics
+chickwts_lm <- lm(weight ~ feed , data =  chickwts)
+chickwts_lm
+anova(chickwts_lm)
+
+
+# Rate of change
+chickwts_lm <- chickwts_lm %>% 
+  chickwts_lm <- (diff [weight ~ feed] - value[data =  chickwts]) %>% 
+  arrange(-diff)
+
+
+chickwts.av <- aov(weight ~ feed, data = chickwts)
 
 
 #measure and compare feed supplements on the growth rate of chickens
